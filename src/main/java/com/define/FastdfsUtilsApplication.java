d2ca1ca9e2de3213d57a636c4b94177d21da088b
liguocheng <ligc@gzdefine.com>
2018-11-05 09:25:40 +0800

    第一次

---
+++ src/main/java/com/define/FastdfsUtilsApplication.java
@@ -0,0 +1,14 @@
+package com.define;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastdfsUtilsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FastdfsUtilsApplication.class, args);
+		System.out.println("启动成功");
+	}
+}
